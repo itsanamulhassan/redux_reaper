e266e95608ba8122dccb06477f92c6c05eb503ba
Anamul Hassan <itsanamulhassan@gmail.com>
2025-06-28 17:41:09 +0600

    feat: initialize counter app with Redux and Vite
    - Added package.json with dependencies for React, Redux, and Vite.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application layout.
    - Developed App.jsx component with increment and decrement buttons.
    - Added React logo SVG in assets directory.
    - Established global styles in index.css.
    - Set up main entry point in main.jsx with React.StrictMode and Redux Provider.
    - Configured Redux store in store.js with a placeholder reducer.
    - Configured Vite in vite.config.js for project setup.

---
+++ module21/counterApp/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Counter with Redux</h1>
+
+      <ul className="buttons">
+        <li>
+          <button>Increment</button>
+        </li>
+        <li>
+          <h2>0</h2>
+        </li>
+        <li>
+          <button>Decrement</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
