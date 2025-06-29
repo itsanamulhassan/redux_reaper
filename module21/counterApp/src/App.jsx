5bc18e2c08ffea825be3824bbe38ffbcf0610ee4
Anamul Hassan <itsanamulhassan@gmail.com>
2025-06-29 15:38:56 +0600

    feat: implement counter functionality with increment and decrement actions

--- module21/counterApp/src/App.jsx
+++ module21/counterApp/src/App.jsx
@@ -1,19 +1,36 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
   return (
     <div>
       <h1>Counter with Redux</h1>
 
       <ul className="buttons">
         <li>
-          <button>Increment</button>
+          <button
+            onClick={() => {
+              dispatch(increment());
+            }}
+          >
+            Increment
+          </button>
         </li>
         <li>
-          <h2>0</h2>
+          <h2>{state.counter.count}</h2>
         </li>
         <li>
-          <button>Decrement</button>
+          <button
+            onClick={() => {
+              dispatch(decrement());
+            }}
+          >
+            Decrement
+          </button>
         </li>
       </ul>
     </div>
